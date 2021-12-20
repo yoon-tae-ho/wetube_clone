65219c74bc137bb0a85a3b562c52d69503d5839c
yoontaeho <pln0302@yonsei.ac.kr>
2021-12-20 16:15:16 +0900

    Learned Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,50 +1,31 @@
-const fakeVideos = [
-  {
-    title: "first video",
-    ratings: 5,
-    comments: 2,
-    createdAt: "2 years ago",
-    views: 109,
-    id: 1,
-  },
-  {
-    title: "second video",
-    ratings: 5,
-    comments: 2,
-    createdAt: "2 years ago",
-    views: 109,
-    id: 2,
-  },
-  {
-    title: "third video",
-    ratings: 5,
-    comments: 2,
-    createdAt: "2 years ago",
-    views: 109,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
 // For globalRouters
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeVideos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (err) {
+    console.error(err);
+  }
+};
 export const search = (req, res) => res.send("Search!");
 
 // For videoRouters
 export const watchVideo = (req, res) => {
   const { id } = req.params;
-  const video = fakeVideos[id - 1];
-  res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+
+  res.render("watch", { pageTitle: `Watching:` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = fakeVideos[id - 1];
-  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+
+  res.render("edit", { pageTitle: `Editing:` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  fakeVideos[id - 1].title = title;
+
   res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -52,14 +33,6 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    ratings: 0,
-    comments: 0,
-    createdAt: "now",
-    views: 0,
-    id: fakeVideos.length + 1,
-  };
-  fakeVideos.push(newVideo);
+
   return res.redirect("/");
 };
