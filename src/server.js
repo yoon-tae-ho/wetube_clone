456d23f8eb7357bc018f909c18ae85d171d5b4b7
yoontaeho <pln0302@yonsei.ac.kr>
2021-12-20 13:51:26 +0900

    Divided init.js from server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -22,7 +18,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
