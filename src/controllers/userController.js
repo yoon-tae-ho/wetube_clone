498cf9c6b1880ba89852d55f6eece8b3c6fdeda3
yoontaeho <pln0302@yonsei.ac.kr>
2021-12-27 04:19:29 +0900

    Added login part

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
@@ -6,16 +7,17 @@ export const getJoin = (req, res) => {
 export const postJoin = async (req, res) => {
   try {
     const { name, email, password, password2, username, location } = req.body;
+    const pageTitle = "Join";
     if (password !== password2) {
       return res.render("join", {
-        pageTitle: "Join",
+        pageTitle,
         errorMessage: "Password confirmation dose not match.",
       });
     }
     const userExist = await User.exists({ $or: [{ username }, { email }] });
     if (userExist) {
       return res.status(400).render("join", {
-        pageTitle: "Join",
+        pageTitle,
         errorMessage: "The username/email already exists.",
       });
     }
@@ -29,9 +31,42 @@ export const postJoin = async (req, res) => {
     return res.redirect("/login");
   } catch (err) {
     console.error(err);
+    return res
+      .status(404)
+      .render("join", { pageTitle: "Unexpected error occured." });
   }
 };
-export const login = (req, res) => res.send("Login!");
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  try {
+    const user = await User.findOne({ username });
+    // Username validation
+    if (!user) {
+      return res.status(400).render("login", {
+        pageTitle,
+        errorMessage: "An account with this username dose not exists.",
+      });
+    }
+    // Password validation
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).render("login", {
+        pageTitle,
+        errorMessage: "Password does not correct.",
+      });
+    }
+    console.log("Log In WOW!!!!!!!!!");
+    return res.end();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export const editUser = (req, res) => res.send("Edit User!");
 export const deleteUser = (req, res) => res.send("Delete User!");
 export const seeUser = (req, res) => res.send("See user!");
