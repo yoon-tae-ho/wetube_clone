941152bd2a2ad70e90b19ee52dced9858fdd1296
yoontaeho <pln0302@yonsei.ac.kr>
2022-01-01 23:13:44 +0900

    Added protector and public only middlewares

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,12 +7,21 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middelwares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-z]{24})", watchVideo);
-videoRouter.route("/:id([0-9a-z]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id([0-9a-z]{24})/delete", deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-z]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter.get("/:id([0-9a-z]{24})/delete", protectorMiddleware, deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
