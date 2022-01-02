6c198a3d28161f91f21e8227817b4802d7dc57c4
yoontaeho <pln0302@yonsei.ac.kr>
2022-01-02 19:57:11 +0900

    Fixed the bug of multiple hasing password

--- src/models/User.js
+++ src/models/User.js
@@ -15,7 +15,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
